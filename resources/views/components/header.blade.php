a021de0d71182ebc716be59e99c93f0142092d4c
MH IT Bangladesh <35065634+Coder-fox-bd@users.noreply.github.com>
2022-02-23 12:58:38 +0600

    commit 1.1.4

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -20,18 +20,18 @@
         <div class="flex text-md font-bold text-blue-700 lg:flex-grow">
             <a href="#responsive-header"
                 class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 px-4 py-2 rounded mr-2">
-                Browse Fundraising
+                Browse
             </a>
             <div x-data="{ open: false }" class="relative" @click.away="open = false">
                 <button @click="open = !open" class="flex flex-row mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 px-4 py-2 rounded mr-2 font-bold">
-                  <span>Fundrise For</span>
+                  <span>Dropdown For</span>
                   <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-0 font-bold"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
                 <div x-show="open" x-cloak x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute left-0 w-full md:max-w-screen-sm md:w-screen mt-2 origin-top-right z-10">
                   <div class="px-2 pt-2 pb-4 bg-white rounded-md shadow-lg dark:bg-gray-700">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                       <a class="flex row items-start rounded-lg bg-transparent p-2 dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">
-                        <div class="bg-teal-500 text-white rounded-lg p-3">
+                        <div class="bg-teal-500 rounded-lg p-3">
                           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="md:h-6 md:w-6 h-4 w-4"><path d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path></svg>
                         </div>
                         <div class="ml-3">
@@ -41,7 +41,7 @@
                       </a>
       
                       <a class="flex row items-start rounded-lg bg-transparent p-2 dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">
-                        <div class="bg-teal-500 text-white rounded-lg p-3">
+                        <div class="bg-teal-500 rounded-lg p-3">
                           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="md:h-6 md:w-6 h-4 w-4"><path d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
                         </div>
                         <div class="ml-3">
@@ -51,7 +51,7 @@
                       </a>
       
                       <a class="flex row items-start rounded-lg bg-transparent p-2 dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">
-                        <div class="bg-teal-500 text-white rounded-lg p-3">
+                        <div class="bg-teal-500 rounded-lg p-3">
                           <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="md:h-6 md:w-6 h-4 w-4"><path d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path><path d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path></svg>
                         </div>
                         <div class="ml-3">
@@ -63,9 +63,9 @@
                   </div>
                 </div>
             </div>
-            <a href="{{ route('about') }}"
+            <a href="#others"
                 class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-300 px-4 py-2 rounded mr-2">
-                About Us
+                Others
             </a>
         </div>
         <!-- This is an example component -->
@@ -118,11 +118,11 @@
           <a href="/" class="pl-4 text-gray-800 text-xl">Home</a>
         </div>
         <div class="flex flex-row mt-4 lg:inline-block lg:mt-0 px-4 py-2 rounded mr-2 font-bold">
-          <a href="#" class="pl-4 text-gray-800 text-xl">Browse Fundraising</a>
+          <a href="#" class="pl-4 text-gray-800 text-xl">Browse</a>
         </div>
         <div x-data="{ open: false }" class="relative" @click.away="open = false">
           <button @click="open = !open" class="flex flex-row mt-4 lg:inline-block lg:mt-0 px-4 py-2 rounded mr-2 font-bold">
-            <span class="pl-4 text-gray-800 text-xl">Fundrise For</span>
+            <span class="pl-4 text-gray-800 text-xl">Dropdown For</span>
             <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-5 h-5 mt-1 ml-1 transition-transform duration-200 transform md:-mt-0 font-bold text-gray-800"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
           </button>
           <div x-show="open" x-cloak x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute left-0 w-full md:max-w-screen-sm md:w-screen mt-2 origin-top-right px-3 z-10">
@@ -162,7 +162,7 @@
           </div>
       </div>
       <div class="flex flex-row mt-4 lg:inline-block lg:mt-0 px-4 py-2 rounded mr-2 font-bold">
-        <a href="{{ route('about') }}" class="pl-4 text-gray-800 text-xl">About Us</a>
+        <a href="#others" class="pl-4 text-gray-800 text-xl">Others</a>
       </div>
       <div class="text-center">
         @if (Route::has('login'))
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,7 @@
 <x-guest-layout>
-    {{-- <div style="background: rgba(37, 35, 97, 1);">
-        <x-home-top />
-        <x-trending-fundriser />
-        <x-fundrising-steps />
-        <x-fundrising-causes />
-        <x-why-us />
-        <x-success-story />
-    </div> --}}
+    <div class="h-screen flex items-center justify-center">
+        <div class="text-3xl">
+            Welcome to Laravel Jetstream CMS
+        </div>
+    </div>
 </x-guest-layout>
